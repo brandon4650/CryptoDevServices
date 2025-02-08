a55bdb1b1c519c2cb70ece38640c17ceab2e7c00
sir.wiggles <116675066+brandon4650@users.noreply.github.com>
2025-02-07 23:00:38 -0500

    Create packageData.js

---
+++ src/utils/packageData.js
@@ -0,0 +1,23 @@
+export const SELL_APP_PACKAGES = [
+  {
+    storeId: "56234",
+    productId: "273869",
+    planName: "Basic Plan",
+    price: "150",
+    description: "Basic Website Design with Essential Features"
+  },
+  {
+    storeId: "56234",
+    productId: "273879",
+    planName: "Standard Plan",
+    price: "300",
+    description: "Advanced Design with Interactive Elements"
+  },
+  {
+    storeId: "56234",
+    productId: "273880",
+    planName: "Premium Plan",
+    price: "450",
+    description: "Full-Featured Website with Premium Integrations"
+  }
+];
