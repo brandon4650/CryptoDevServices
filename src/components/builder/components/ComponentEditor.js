7a4f3b81bfe621c3a38e5fde65252515549f0057
sir.wiggles <116675066+brandon4650@users.noreply.github.com>
2025-02-10 00:21:52 -0500

    Update ComponentEditor.js

--- src/components/builder/components/ComponentEditor.js
+++ src/components/builder/components/ComponentEditor.js
@@ -24,7 +24,7 @@ const ComponentEditor = ({ section, onUpdate, onClose }) => {
               <input
                 type="text"
                 value={section.data.title}
-                onChange={(e) => updateField('title', e.target.value)})}
+                onChange={(e) => updateField('title', e.target.value)}
                 className="w-full bg-blue-900/30 border border-blue-800/50 rounded-lg px-4 py-2 text-white focus:outline-none focus:border-cyan-500"
               />
             </div>
