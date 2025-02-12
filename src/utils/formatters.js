42fd178612d92d8b0fdc4e90dfe3e2106450c3a3
sir.wiggles <116675066+brandon4650@users.noreply.github.com>
2025-02-11 21:55:50 -0500

    Create formatters.js

---
+++ src/utils/formatters.js
@@ -0,0 +1,13 @@
+// src/utils/formatters.js
+export const formatNumber = (num) => {
+  if (num >= 1e9) {
+    return (num / 1e9).toFixed(2) + 'B';
+  }
+  if (num >= 1e6) {
+    return (num / 1e6).toFixed(2) + 'M';
+  }
+  if (num >= 1e3) {
+    return (num / 1e3).toFixed(2) + 'K';
+  }
+  return num.toFixed(2);
+};
